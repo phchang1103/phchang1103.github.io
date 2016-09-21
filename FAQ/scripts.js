75da9eeef8e7087885ab225f8a1fb77d8b67968c
phchang1103 <phchang1103@gmail.com>
2016-09-21 22:18:20 +0800

    0921
    FAQ done

---
+++ FAQ/scripts.js
@@ -0,0 +1,69 @@
+
+var scroll = function() {
+
+  $(window).scroll(function(){
+
+  if ($(this).scrollTop() > 100 && $(this).width() > 1136){
+	    $('header').addClass("headersmall");
+      $('#logo').addClass('logosmall');
+      $('header nav').css('display', 'none');
+      $('#menu').css('display', 'inline-block');
+     }
+  else if ($(this).width() < 1136) {
+      $('header nav').css('display', 'none');
+     }
+	else {
+     $('header').removeClass("headersmall");
+     $('#logo').removeClass('logosmall');
+     $('header nav').css('display', 'inline-block');
+     $('#menu').css('display', 'none');
+      	}
+   });
+}
+
+
+var sidenav = function() {
+
+     $('#menu').on('click', function() {
+       $('#sidenav').css('width', '200px');
+       $('#page-mask').css('display', 'block');
+     });
+
+     $('#page-mask').on('click', function() {
+       $('#sidenav').css('width', '0');
+       $('#page-mask').css('display', 'none');
+     })
+
+     $('#closemenu').on('click', function() {
+       $('#sidenav').css('width', '0');
+       $('#page-mask').css('display', 'none');
+     });
+
+}
+
+var drawer = function() {
+     $('.Q').on('click', function() {
+       var targetA = $(this).next();
+
+       if ( targetA.height() == '0' ) {
+         $('.A').animate({height: 0}, 100, 'linear');
+         $('.A').animate({margin: 0}, 100, 'linear');
+         var autoHeight = targetA.css('height', 'auto').height();
+         targetA.animate({margin: '20px 0'}, 200, 'linear');
+         targetA.height(0).animate({height: autoHeight}, 200, 'linear');
+       }
+       else {
+         $('.A').animate({height: 0}, 100, 'linear');
+         $('.A').animate({margin: 0}, 100, 'linear');         
+       }
+
+
+
+     });
+}
+
+
+
+$(document).ready(scroll);
+$(document).ready(sidenav);
+$(document).ready(drawer);
